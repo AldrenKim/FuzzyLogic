3269b6303dec18a5cf63cab85e5355d5be6696e5
AldrenKim <69776513+AldrenKim@users.noreply.github.com>
2021-03-07 16:27:46 +0800

    A

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DotFuzzy;
 
 namespace FuzzyLogic
 {
@@ -15,6 +16,36 @@ namespace FuzzyLogic
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LinguisticVariable water = new LinguisticVariable("Water");
+            water.MembershipFunctionCollection.Add(new MembershipFunction("Cold", 0, 0, 20, 40));
+            water.MembershipFunctionCollection.Add(new MembershipFunction("Tepid", 30, 50, 50, 70));
+            water.MembershipFunctionCollection.Add(new MembershipFunction("Hot", 50, 80, 100, 100));
+
+            LinguisticVariable power = new LinguisticVariable("Power");
+            power.MembershipFunctionCollection.Add(new MembershipFunction("Low", 0, 25, 25, 50));
+            power.MembershipFunctionCollection.Add(new MembershipFunction("High", 25, 50, 50, 75));
+
+            FuzzyEngine fuzzyEngine = new FuzzyEngine();
+            fuzzyEngine.LinguisticVariableCollection.Add(water);
+            fuzzyEngine.LinguisticVariableCollection.Add(power);
+            fuzzyEngine.Consequent = "Power";
+            fuzzyEngine.FuzzyRuleCollection.Add(new FuzzyRule("IF (Water IS Cold) OR (Water IS Tepid) THEN Power IS High"));
+            fuzzyEngine.FuzzyRuleCollection.Add(new FuzzyRule("IF (Water IS Hot) THEN Power IS Low"));
+
+            water.InputValue = 60;
+
+            try
+            {
+                MessageBox.Show(fuzzyEngine.Defuzzify().ToString());
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
         }
     }
 }
