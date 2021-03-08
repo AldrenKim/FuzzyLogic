29e7e39906bbee4a5cfb9211abcc23aa42458e70
AldrenKim <69776513+AldrenKim@users.noreply.github.com>
2021-03-08 22:27:42 +0800

    Final?

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,100 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(164, 67);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(164, 108);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(164, 151);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(480, 63);
+            this.button1.Location = new System.Drawing.Point(156, 197);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 73);
+            this.label1.Location = new System.Drawing.Point(80, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Text = "Altitude";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(86, 115);
+            this.label2.Location = new System.Drawing.Point(80, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "Wind Speed (kts)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(86, 158);
+            this.label3.Location = new System.Drawing.Point(80, 131);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Text = "Speed";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(188, 49);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            45000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(78, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(188, 85);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            29,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(78, 20);
+            this.numericUpDown2.TabIndex = 13;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(188, 129);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(78, 20);
+            this.numericUpDown3.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(705, 314);
+            this.ClientSize = new System.Drawing.Size(401, 262);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Flight Simulator?";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,34 +13,139 @@ namespace FuzzyLogic
 {
     public partial class Form1 : Form
     {
-        LinguisticVariable dSpeed;
-        LinguisticVariable dAltitude;
-        LinguisticVariable dWind;
+        LinguisticVariable dSpeed, dAltitude, dWind, dLand;
+        MembershipFunctionCollection wind, altitude, speed, land;
+        FuzzyEngine fuzz;
+        OutputForm newForm;
+        public static Image outputImage;
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+                fuzz = new FuzzyEngine();
+                AddMembers();
+                SetRules();
+            newForm = new OutputForm();
+            double res = fuzz.Defuzzify();
+            if (res < 4.0)
+                outputImage = Properties.Resources.dont;
+            else if (res < 7.0)
+                outputImage = Properties.Resources.could;
+            else
+                outputImage = Properties.Resources.go;
+            newForm.ShowImage(outputImage);
+            newForm.Show();           
+        }
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        public AddMembers()
+        public void AddMembers()
         {
-            MembershipFunctionCollection wind = new MembershipFunctionCollection();
-            wind.Add(new MembershipFunction("WEAK", 0.0, 0.0, 5.0, 10.0));
-            wind.Add(new MembershipFunction("MEDIUM", 8.0, 8.0, 15.0, 18.0));
-            wind.Add(new MembershipFunction("STRONG", 15.0, 15.0, 25.0, 29.0));
+            wind = new MembershipFunctionCollection();
+            wind.Add(new MembershipFunction("WEAK", 0.0, 7.0, 7.0, 10.0));
+            wind.Add(new MembershipFunction("MEDIUM", 8.0, 15.0, 15.0, 18.0));
+            wind.Add(new MembershipFunction("STRONG", 16.0, 25.0, 25.0, 29.0));
+            dWind = new LinguisticVariable("Wind", wind);
 
+            altitude = new MembershipFunctionCollection();
+            altitude.Add(new MembershipFunction("LOW", 0.0,12000.0, 12000.0, 15000.0));
+            altitude.Add(new MembershipFunction("OK", 13000.0, 25000.0, 25000.0, 35000.0));
+            altitude.Add(new MembershipFunction("HIGH", 33000.0, 40000.0, 40000.0, 45000.0));
+            dAltitude = new LinguisticVariable("Altitude", altitude);
 
-            MembershipFunctionCollection altitude = new MembershipFunctionCollection();
-            altitude.Add(new MembershipFunction("LOW", 0.0, 0.0, 12000.0, 15000.0));
-            altitude.Add(new MembershipFunction("OK", 13000.0, 13000.0, 25000.0, 35000.0));
-            altitude.Add(new MembershipFunction("HIGH", 33000.0, 33000.0, 45000.0, 50000.0));
+            speed = new MembershipFunctionCollection();
+            speed.Add(new MembershipFunction("SLOW", 0.0, 75.0, 75.0, 115.0));
+            speed.Add(new MembershipFunction("FINE", 100.0, 180.0, 180.0, 230.0));
+            speed.Add(new MembershipFunction("FAST", 215.0, 260.0, 260.0, 300.0));
+            dSpeed = new LinguisticVariable("Speed", speed);
 
-            MembershipFunctionCollection speed = new MembershipFunctionCollection();
-            speed.Add(new MembershipFunction("SLOW", 0.0, 0.0, 12000.0, 15000.0));
-            speed.Add(new MembershipFunction("FINE", 13000.0, 13000.0, 25000.0, 35000.0));
-            speed.Add(new MembershipFunction("FAST", 33000.0, 33000.0, 45000.0, 50000.0));
+            land = new MembershipFunctionCollection();
+            land.Add(new MembershipFunction("UNABLE", 0.0, 2.0, 2.0, 4.0));
+            land.Add(new MembershipFunction("COULD", 3.0, 5.0, 5.0, 7.0));
+            land.Add(new MembershipFunction("SHOULD", 6.0, 9.0, 9.0, 10.0));
+            dLand = new LinguisticVariable("Land", land);
+
         }
 
+        public void SetRules()
+        {
+            fuzz.LinguisticVariableCollection.Add(dWind);
+            fuzz.LinguisticVariableCollection.Add(dAltitude);
+            fuzz.LinguisticVariableCollection.Add(dSpeed);
+            fuzz.LinguisticVariableCollection.Add(dLand);
+            fuzz.Consequent = "Land";
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS LOW) AND (Speed IS SLOW) THEN Land IS COULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS LOW) AND (Speed IS FINE) THEN Land IS SHOULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS LOW) AND (Speed IS FAST) THEN Land IS UNABLE"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS OK) AND (Speed IS SLOW) THEN Land IS SHOULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS OK) AND (Speed IS FINE) THEN Land IS SHOULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS OK) AND (Speed IS FAST) THEN Land IS SHOULD"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS HIGH) AND (Speed IS SLOW) THEN Land IS UNABLE"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS HIGH) AND (Speed IS FINE) THEN Land IS SHOULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS WEAK) AND (Altitude IS HIGH) AND (Speed IS FAST) THEN Land IS SHOULD"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS LOW) AND (Speed IS SLOW) THEN Land IS COULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS LOW) AND (Speed IS FINE) THEN Land IS SHOULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS LOW) AND (Speed IS FAST) THEN Land IS SHOULD"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS OK) AND (Speed IS SLOW) THEN Land IS COULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS OK) AND (Speed IS FINE) THEN Land IS SHOULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS OK) AND (Speed IS FAST) THEN Land IS SHOULD"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS HIGH) AND (Speed IS SLOW) THEN Land IS UNABLE"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS HIGH) AND (Speed IS FINE) THEN Land IS SHOULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS MEDIUM) AND (Altitude IS HIGH) AND (Speed IS FAST) THEN Land IS SHOULD"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS LOW) AND (Speed IS SLOW) THEN Land IS UNABLE"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS LOW) AND (Speed IS FINE) THEN Land IS COULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS LOW) AND (Speed IS FAST) THEN Land IS COULD"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS OK) AND (Speed IS SLOW) THEN Land IS UNABLE"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS OK) AND (Speed IS FINE) THEN Land IS COULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS OK) AND (Speed IS FAST) THEN Land IS SHOULD"));
+
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS HIGH) AND (Speed IS SLOW) THEN Land IS UNABLE"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS HIGH) AND (Speed IS FINE) THEN Land IS COULD"));
+            fuzz.FuzzyRuleCollection.Add(new FuzzyRule("IF (Wind IS STRONG) AND (Altitude IS HIGH) AND (Speed IS FAST) THEN Land IS SHOULD"));
+
+
+            GetData();
+        }
+
+
+        public void GetData()
+        {
+            dWind.InputValue = Convert.ToDouble(numericUpDown2.Value);
+            dWind.Fuzzify("MEDIUM");
+            /*if (dWind.InputValue < 10.0)
+                dWind.Fuzzify("WEAK");
+            else if (dWind.InputValue < 18.0)
+                dWind.Fuzzify("MEDIUM");
+            else
+                dWind.Fuzzify("STRONG");*/
+
+
+            dAltitude.InputValue = Convert.ToDouble(numericUpDown1.Value);
+            dAltitude.Fuzzify("HIGH");
+            /*if (dAltitude.InputValue < 15000.0)
+                dAltitude.Fuzzify("LOW");
+            else if (dAltitude.InputValue < 35000.0)
+                dAltitude.Fuzzify("OK");
+            else
+                dAltitude.Fuzzify("HIGH");*/
+
+            dSpeed.InputValue = Convert.ToDouble(numericUpDown3.Value);
+            dSpeed.Fuzzify("FINE");
+            /*if (dSpeed.InputValue < 115.0)
+                dSpeed.Fuzzify("SLOW");
+            else if (dSpeed.InputValue < 230.0)
+                dSpeed.Fuzzify("FINE");
+            else
+                dSpeed.Fuzzify("FAST");*/
+        }
     }
 }
